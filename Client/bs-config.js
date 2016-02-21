938106ae912778a018560345e2f999abe3f3da84
RizkiMufrizal <mufrizalrizki@gmail.com>
2016-02-21 22:30:50 +0700

    tambah konfigurasi browserify

---
+++ Client/bs-config.js
@@ -0,0 +1,24 @@
+var proxyMiddleware = require('http-proxy-middleware');
+var historyFallback = require('connect-history-api-fallback');
+var log = require('connect-logger');
+
+var proxy = proxyMiddleware('/api', {
+  target: 'http://localhost:1337/'
+});
+
+module.exports = {
+  injectChanges: false,
+  files: ['./**/*.{html,htm,css,js}'],
+  server: {
+    baseDir: './',
+    middleware: [
+      proxy,
+      log({
+        format: '%date %status %method %url'
+      }),
+      historyFallback({
+        "index": '/index.html'
+      })
+    ]
+  }
+};
