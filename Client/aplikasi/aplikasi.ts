28c4d480ea6212b342bd3d603d8317d8d5d58333
RizkiMufrizal <mufrizalrizki@gmail.com>
2016-02-21 22:31:14 +0700

    tambah root baru

--- Client/aplikasi/aplikasi.ts
+++ Client/aplikasi/aplikasi.ts
@@ -1,13 +1,16 @@
 import {Component} from 'angular2/core';
 import {RouteConfig, ROUTER_DIRECTIVES} from 'angular2/router';
 import {HaloComponent} from './halo/halo.component';
+import {BarangComponent} from './barang/barang.component';
 
 @Component({
     selector: 'aplikasi',
     templateUrl: './aplikasi/aplikasi.html',
-    directives: [ROUTER_DIRECTIVES, HaloComponent]
+    directives: [ROUTER_DIRECTIVES]
 })
 @RouteConfig([
-
+    {
+        path: '/', name: 'BarangComponent', component: BarangComponent, useAsDefault: true
+    }
 ])
 export class AplikasiComponent { }
