d14959c9240686abe5101d01433fe5db5c1e1d19
RizkiMufrizal <mufrizalrizki@gmail.com>
2016-02-27 18:06:24 +0700

    update nama variabel dan tambah update, dan delete

--- Client/aplikasi/barang/barang.list.component.ts
+++ Client/aplikasi/barang/barang.list.component.ts
@@ -11,28 +11,67 @@ import {BarangService} from './barang.service';
 })
 export class BarangListComponent {
 
-    public daftarBarang: any;
-    public inputDaftarBarang = {};
+    public _daftarBarang: any;
+    public _inputDaftarBarang: any = {};
+    public _enable: boolean;
 
-    constructor(private bs: BarangService) {
+    constructor(private _barangService: BarangService) {
         this.loadBarang();
     }
 
     loadBarang() {
-        let hasil: Observable<Response> = this.bs.getDaftarBarang();
+        let hasil: Observable<Response> = this._barangService.getDaftarBarang();
 
         hasil.subscribe(
-            (data) => { this.daftarBarang = data },
+            (data) => { this._daftarBarang = data },
             (err) => { console.log(err) },
             () => { console.log("request selesai") }
         );
     }
 
-    simpanDaftarBarang(barang: Array<any>) {
-        this.bs.simpanDaftarBarang(barang).subscribe(
+    tambahDaftarBarang() {
+        this._enable = false;
+        this._inputDaftarBarang = {};
+    }
+
+    simpanDaftarBarang(_barang: Array<any>) {
+        this._barangService.simpanDaftarBarang(_barang).subscribe(
             (data) => { console.log(data.json) },
             (err) => { console.log(err) },
-            () => { this.loadBarang() }
+            () => {
+                this.loadBarang();
+                this._inputDaftarBarang = {};
+            }
         );
     }
+
+    editDaftarBarang(_barang: any) {
+        this._enable = true;
+        this._inputDaftarBarang.idBarang = _barang.idBarang;
+        this._inputDaftarBarang.namaBarang = _barang.namaBarang;
+        this._inputDaftarBarang.jenisBarang = _barang.jenisBarang;
+        this._inputDaftarBarang.tanggalKadaluarsa = _barang.tanggalKadaluarsa;
+    }
+
+    updateDaftarBarang(_barang: Array<any>) {
+        this._barangService.updateDaftarBarang(_barang).subscribe(
+            (data) => { console.log(data.json) },
+            (err) => { console.log(err) },
+            () => {
+                this.loadBarang();
+                this._inputDaftarBarang = {};
+            }
+        );
+    }
+
+    hapusDaftarBarang(idBarang: string) {
+        this._barangService.hapusDaftarBarang(idBarang).subscribe(
+            (data) => { console.log(data.json) },
+            (err) => { console.log(err) },
+            () => {
+                this.loadBarang();
+            }
+        );
+    }
+
 }
